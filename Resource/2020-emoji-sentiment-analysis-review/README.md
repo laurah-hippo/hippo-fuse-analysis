ea64cdb2ceb5e36aa15089e8ce23329706bbaa1f
Laura Holley <laura.holley@hippodigital.com>
2026-09-16 16:54:31 +0100

    Many changes, mainly testing the processing of one screenshot

---
+++ Resource/2020-emoji-sentiment-analysis-review/README.md
@@ -0,0 +1,8 @@
+# Sentiment Analysis from WhatsApp Emojis
+In 2020, github user Subhasishbasak explored the possiblity of identifying how their friends were feeling through sentiment analysis based on screenshots of the friends' most used WhatsApp emojis.
+
+The project was a success and the code was shared on github - [emoji sentiment analysis](https://github.com/Subhasishbasak/emoji_analysis)
+
+Whilst the outcome of this sentiment analysis project is slightly different to the goals of FUSE Analysis, it is useful to reference and use as a guide for ways to extract individual emoji images and matching against the Slack emoji list.
+
+This section goes through the code used in the emoji sentiment analysis to understand what it is doing and what can be reused as part of the FUSE task.
---
+++ Resource/2020-emoji-sentiment-analysis-review/image_to_emoji.py
@@ -0,0 +1,76 @@
+# takes the process-able image as input outputs the list of emojis as a list of np arrays <- Original explanation of the code
+
+import cv2 # a module as part of OpenCV, an open-source computer vision library used for image processing, real-time computer vision applications, and machine learning tasks.
+import statistics # a module that provides functions for calculating mathematical statistics of numeric data, such as mean, median, mode, variance, and standard deviation.
+import numpy as np
+from scipy.signal import find_peaks # finds peaks inside a signal based on peak properties. Finds all local maxima by simple comparison of neighboring values.
+
+def image_2_emoji(image):
+    
+    def to_half(image):
+        
+        n_col = image.shape[1]//2
+
+        img_left = image[:, :n_col]
+        img_right = image[:, n_col:]
+
+        return (img_left, img_right)
+
+    def extract_from_half(image):
+        
+        emoji_list = []
+        for i in range(4):
+            for j in range(4):
+                temp = image[i*70:(i+1)*70,j*70:(j+1)*70]
+                emoji_list.append(temp)
+        return emoji_list
+    
+    img = image
+    
+    dim = (560,280)
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA) 
+    
+    halfed = to_half(resized)
+    
+    output = extract_from_half(halfed[0])
+    output += extract_from_half(halfed[1])
+    
+    return output
+
+
+def image_2_emoji_2(image):
+    img_1 = image.copy()
+    
+    kernel = np.ones((5,5),np.float32)/25
+    k = 20
+    for i in range(k):
+        image = cv2.filter2D(image,-1,kernel)
+        
+    # Row check
+    peak_len = []
+    l = np.random.uniform(low=0, high=image.shape[0], size=1000).astype(int)
+    for i in l:
+        pixel_array = image[:,:,2][i]
+        peaks, val = find_peaks(pixel_array, height=0)
+        peak_len.append(len(peaks))
+    num_emojis_row = statistics.mode(peak_len)
+    
+    # Column check
+    peak_len = []
+    l = np.random.uniform(low=0, high=image.shape[0]//2, size=1000).astype(int)
+    for i in l:
+        pixel_array = image[:,:,2][:, i]
+        peaks, val = find_peaks(pixel_array, height=0)
+        peak_len.append(len(peaks))
+    num_emojis_col = statistics.mode(peak_len)
+    
+    dim = (70*num_emojis_row,70*num_emojis_col)    
+    img_1 = cv2.resize(img_1, dim, interpolation = cv2.INTER_AREA) 
+    
+    emoji_list = []
+    for j in range(num_emojis_row):
+        for i in range(num_emojis_col):
+            temp = img_1[i*70:(i+1)*70,j*70:(j+1)*70]
+            emoji_list.append(temp)
+    
+    return emoji_list
